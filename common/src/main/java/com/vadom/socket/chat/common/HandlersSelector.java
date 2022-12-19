1c2c057374f78be7989e93033121a4cbfbf26e43
Vado101 <vadmayer101@gmail.com>
2022-12-19 17:47:31 +0200

    Improved HandlerSelector and added test for him

--- common/src/main/java/com/vadom/socket/chat/common/HandlersSelector.java
+++ common/src/main/java/com/vadom/socket/chat/common/HandlersSelector.java
@@ -9,16 +9,22 @@ public class HandlersSelector {
     private final List<Handler> handlers =
             Collections.synchronizedList(new LinkedList<>());
     private final Queue<Handler> handlerQueue = new LinkedList<>();
-    private boolean quitFlag;
+    private boolean isRun;
 
     public void add(Handler handler) {
-        handlerQueue.add(Objects.requireNonNull(handler));
+        Objects.requireNonNull(handler);
+
+        if (isRun) {
+            handlerQueue.add(handler);
+        } else {
+            handlers.add(handler);
+        }
     }
 
     public void run() {
-        quitFlag = false;
+        isRun = true;
 
-        while (!quitFlag) {
+        while (isRun) {
             ListIterator<Handler> iterator = handlers.listIterator();
 
             while (!handlerQueue.isEmpty()) {
@@ -38,6 +44,19 @@ public class HandlersSelector {
     }
 
     public void stop() {
-        quitFlag = true;
+        isRun = false;
+    }
+
+    public int getFreeID() {
+        int id = 0;
+
+        for (int i = 0; i < handlers.size(); ++i) {
+            if (handlers.get(i).getId() == id) {
+                ++id;
+                i = -1;
+            }
+        }
+
+        return id;
     }
 }
---
+++ common/src/test/java/com/vadom/socket/chat/common/HandlersSelectorTest.java
@@ -0,0 +1,43 @@
+package com.vadom.socket.chat.common;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HandlersSelectorTest {
+
+    @Test
+    void addTest() {
+        HandlersSelector handlersSelector = new HandlersSelector();
+        Assertions.assertThrows(NullPointerException.class,
+                () -> handlersSelector.add(null));
+    }
+
+    @Test
+    void getFreeID() {
+        HandlersSelector handlersSelector = new HandlersSelector();
+        int startID = 0;
+
+        Assertions.assertEquals(startID, handlersSelector.getFreeID());
+
+        Handler handler1 = createHandler(startID);
+        Handler handler2 = createHandler(++startID);
+        handlersSelector.add(handler1);
+        handlersSelector.add(handler2);
+
+        Assertions.assertEquals(++startID, handlersSelector.getFreeID());
+
+        Handler handler3 = createHandler(++startID);
+        handlersSelector.add(handler3);
+
+        Assertions.assertEquals(--startID, handlersSelector.getFreeID());
+    }
+
+    private Handler createHandler(int id) {
+        return new Handler(id) {
+            @Override
+            public void handle() {
+                System.out.println("handler with ID=" + getId());
+            }
+        };
+    }
+}
